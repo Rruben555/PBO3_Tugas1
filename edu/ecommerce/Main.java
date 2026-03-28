b13042bd211f2622a2c73e5c1d1fb6c136551653
Rruben555 <rubensiregar25@gmail.com>
2026-03-28 10:31:38 +0700

    Update Main.java

--- edu/ecommerce/Main.java
+++ edu/ecommerce/Main.java
@@ -1,22 +1,14 @@
 package edu.ecommerce;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args){
-        ArrayList<Order.OrderItem> items=new ArrayList<Order.OrderItem>();
+        Order.OrderItem[] items = new Order.OrderItem[3];
         Order order=new Order("1", "Andi");
 
-        Order.OrderItem item1=new Order.OrderItem("Le Minerale",3000,2);
-        Order.OrderItem item2=new Order.OrderItem("Oreo",6000,4);
-        Order.OrderItem item3=new Order.OrderItem("Indomie", 3500, 10);
+        items[0]=new Order.OrderItem("Le Minerale",3000,2);
+        items[1]=new Order.OrderItem("Oreo",6000,4);
+        items[2]=new Order.OrderItem("Indomie", 3500, 10);
 
-        items.add(item1);
-        items.add(item2);
-        items.add(item3);
-
-        order.getInfo(items);
-
-
+        order.tampilkanRincian(items);
     }
 }
