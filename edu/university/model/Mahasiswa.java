d49ec2a2623049a0520bbc9932c219b33a5841eb
Rruben555 <rubensiregar25@gmail.com>
2026-04-03 15:31:36 +0700

    Update Mahasiswa.java

--- edu/university/model/Mahasiswa.java
+++ edu/university/model/Mahasiswa.java
@@ -1,5 +1,4 @@
 package edu.university.model;
-import java.util.ArrayList;
 
 
 public class Mahasiswa {
@@ -7,10 +6,39 @@ public class Mahasiswa {
     private String nama;
     private double ipk;
 
+    public Mahasiswa() {
+        this.nim = "-";
+        this.nama = "Belum Diisi";
+        this.ipk = 0.0;
+    }
+
     public Mahasiswa(String nim, String nama, double ipk){
         this.nim=nim;
         this.nama=nama;
         this.ipk=ipk;
+    }
+
+    public void editNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void editNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void editIpk(double ipk) {
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            this.ipk = ipk;
+        } else {
+            System.out.println("Peringatan: IPK " + ipk + " tidak valid!");
+        }
+    }
+
+    public void updateData(String nim, String nama, double ipk) {
+        editNim(nim);
+        editNama(nama);
+        editIpk(ipk);
+        System.out.println("Data mahasiswa telah diperbarui.\n");
     }
 
     public void getKategoriIPK(){
